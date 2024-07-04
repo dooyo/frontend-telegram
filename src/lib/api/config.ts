8723b6498f6bfc097533547be280d7a7ff2b44e3
phDooY <david91.05@mail.ru>
2024-07-05 01:01:15 +0300

    chore: config

--- src/lib/api/config.ts
+++ src/lib/api/config.ts
@@ -1,2 +1,4 @@
-export const API_URL = "http://10.100.102.18:3000";
-export const AUTH_API_URL = "http://10.100.102.18:3001";
+// export const API_URL = 'http://10.100.102.18:3000';
+// export const AUTH_API_URL = 'http://10.100.102.18:3001';
+export const API_URL = 'https://dooyo.koyeb.app';
+export const AUTH_API_URL = 'https://dooyo.koyeb.app/auth';
